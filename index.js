623daec98b0ba6798631b932312b3fdc6d3349c9
Suman599 <chakrabortysuman599@gmail.com>
2024-01-03 12:54:33 +0530

    Light and dark mode toogle with some changes in css

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-   
+let modebtn=document.querySelector("#mode");
+let body=document.querySelector("body");
+let currMode="light";
+
+modebtn.addEventListener("click",()=>{
+    if(currMode==="light")
+    {
+        currMode="dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    }
+    else
+    {
+        currMode="light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+    console.log(currMode);
+});
    
 let menu = document.querySelector('.menu');
 let header = document.querySelector('header');
