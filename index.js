8e7012f5ddacc706ea55a7be5757b3a791232f8c
Suman599 <chakrabortysuman599@gmail.com>
2024-01-04 12:50:08 +0530

    Button modified for light and dark theme

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
-let modebtn=document.querySelector("#mode");
-let body=document.querySelector("body");
-let currMode="light";
-
-modebtn.addEventListener("click",()=>{
-    if(currMode==="light")
-    {
-        currMode="dark";
-        body.classList.add("dark");
-        body.classList.remove("light");
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+    if (localStorageTheme !== null) {
+      return localStorageTheme;
     }
-    else
-    {
-        currMode="light";
-        body.classList.add("light");
-        body.classList.remove("dark");
+  
+    if (systemSettingDark.matches) {
+      return "dark";
     }
-    console.log(currMode);
-});
+  
+    return "light";
+  }
+  
+  
+  function updateButton({ buttonEl, isDark }) {
+    const newCta = isDark ? "Change to light theme" : "Change to dark theme";
+    
+    buttonEl.setAttribute("aria-label", newCta);
+    buttonEl.innerText = newCta;
+  }
+  
+  function updateThemeOnHtmlEl({ theme }) {
+    document.querySelector("html").setAttribute("data-theme", theme);
+  }
+  
+  const button = document.querySelector("[data-theme-toggle]");
+  const localStorageTheme = localStorage.getItem("theme");
+  const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+  
+  let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+  
+  
+  updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
+  updateThemeOnHtmlEl({ theme: currentThemeSetting });
+  button.addEventListener("click", (event) => {
+    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+  
+    localStorage.setItem("theme", newTheme);
+    updateButton({ buttonEl: button, isDark: newTheme === "dark" });
+    updateThemeOnHtmlEl({ theme: newTheme });
+  
+    currentThemeSetting = newTheme;
+  }); 
    
 let menu = document.querySelector('.menu');
 let header = document.querySelector('header');
