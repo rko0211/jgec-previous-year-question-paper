eb25654a1517c7fe57a8aae82b6e78f6929043ed
Suman599 <chakrabortysuman599@gmail.com>
2024-01-07 12:54:28 +0530

    Done changes

--- index.js
+++ index.js
@@ -1,45 +1,15 @@
-function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
-    if (localStorageTheme !== null) {
-      return localStorageTheme;
-    }
-  
-    if (systemSettingDark.matches) {
-      return "dark";
-    }
-  
-    return "light";
+const toggleSwitch = document.getElementById('modeToggle');
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
   }
-  
-  
-  function updateButton({ buttonEl, isDark }) {
-    const newCta = isDark ? "Light theme" : "Dark theme";
-    
-    buttonEl.setAttribute("aria-label", newCta);
-    buttonEl.innerText = newCta;
-  }
-  
-  function updateThemeOnHtmlEl({ theme }) {
-    document.querySelector("html").setAttribute("data-theme", theme);
-  }
-  
-  const button = document.querySelector("[data-theme-toggle]");
-  const localStorageTheme = localStorage.getItem("theme");
-  const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
-  
-  let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
-  
-  
-  updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
-  updateThemeOnHtmlEl({ theme: currentThemeSetting });
-  button.addEventListener("click", (event) => {
-    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
-  
-    localStorage.setItem("theme", newTheme);
-    updateButton({ buttonEl: button, isDark: newTheme === "dark" });
-    updateThemeOnHtmlEl({ theme: newTheme });
-  
-    currentThemeSetting = newTheme;
-  }); 
+}
+
+toggleSwitch.addEventListener('change', switchTheme);
+
    
 let menu = document.querySelector('.menu');
 let header = document.querySelector('header');
