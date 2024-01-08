c72c79720d1d8f8a6b9206fd81071a3198b8e728
Prakash Mondal <97402824+rko0211@users.noreply.github.com>
2024-01-08 23:22:22 +0530

    Revert "Test"

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const toggleSwitch = document.getElementById('modeToggle');
-
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.body.classList.add('dark-mode');
-  } else {
-    document.body.classList.remove('dark-mode');
-  }
-}
-
-toggleSwitch.addEventListener('change', switchTheme);
-
+   
    
 let menu = document.querySelector('.menu');
 let header = document.querySelector('header');
