1c90bda49757922dcf0bac1f333e2942c2161031
Prakash Mondal <2025.cse45.prakashmondal@gmail.com>
2025-05-17 13:26:14 +0530

    Add CSE 8th sem internal-2 paper of 21_25 batch

--- Fourthyear/Seventh_sem_Eight_sem/seventh_sem_eight_sem_question_paper/cse_eighth_sem.js
+++ Fourthyear/Seventh_sem_Eight_sem/seventh_sem_eight_sem_question_paper/cse_eighth_sem.js
@@ -4,7 +4,11 @@ const data = [
     paper: "8th Sem Internal-1",
     link: "7th_sem_8th_sem_pdf/21_25_CSE_8th_Sem_Internal1.pdf"
   },
-
+  {
+    batch: "21-25",
+    paper: "8th Sem Internal-2",
+    link: "7th_sem_8th_sem_pdf/21_25_CSE_8th_Sem_Internal2.pdf"
+  },
   {
     batch: "xx-xx",
     paper: "8th Sem",
