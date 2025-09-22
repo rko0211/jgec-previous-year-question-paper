0666ff2f405ec125c2534a29158e4642b29a889d
Prakash Mondal <2025.cse45.prakashmondal@gmail.com>
2025-09-22 22:10:09 +0530

    Add CE 4th Sem and 4th Sem Internal Paper of 23-27 Batch

--- Secondyear/Third_sem_Fourth_sem/third_sem_fourth_sem_question_paper/ce_fourth_sem.js
+++ Secondyear/Third_sem_Fourth_sem/third_sem_fourth_sem_question_paper/ce_fourth_sem.js
@@ -1,3 +1,4 @@
+// CE 4TH SEM INTERNAL + SEMESTER SECTION
 const data = [
   {
     batch: "20-24",
@@ -9,7 +10,16 @@ const data = [
     paper: "4th Sem",
     link: "4th sem(21-25 batch) - Om Kumar Sah.pdf"
   },
-
+  {
+    batch: "23-27",
+    paper: "4th Sem Internal-I",
+    link: "./3rd_sem_4th_sem pdf/23-27_CE_4th_Sem_Internal - Rajdeep Khatik.pdf"
+  },
+  {
+    batch: "23-27",
+    paper: "4th Sem",
+    link: "./3rd_sem_4th_sem pdf/23_27_CE_4th_Sem - Rajdeep Khatik.pdf"
+  },
   {
     batch: "xx-xx",
     paper: "4th Sem Internal-I",
